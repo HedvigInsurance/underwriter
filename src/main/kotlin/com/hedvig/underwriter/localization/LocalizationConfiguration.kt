f8490624c2f039e1786acfa1a50aac7bcd7dd5a0
Fredrik Tõnisson-Bystam <fredrik.bystam@hedvig.com>
2021-01-27 10:33:34 +0100

    instead of using an extra useFakes boolean, determine Lokalise integration on api key presence

---
+++ src/main/kotlin/com/hedvig/underwriter/localization/LocalizationConfiguration.kt
@@ -0,0 +1,34 @@
+package com.hedvig.underwriter.localization
+
+import com.hedvig.lokalise.client.LokaliseClient
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import java.util.Locale
+
+@Configuration
+class LocalizationConfiguration {
+
+    @Bean
+    @ConditionalOnProperty(
+        value = ["lokalise.projectId", "lokalise.apiToken"]
+    )
+    fun realService(
+        @Value("\${lokalise.projectId}") projectId: String,
+        @Value("\${lokalise.apiToken}") apiToken: String
+    ): LocalizationService {
+        val client = LokaliseClient(projectId, apiToken)
+        return object: LocalizationService {
+            override fun getTranslation(key: String, locale: Locale): String? = client.getTranslation(key, locale)
+        }
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    fun fakeService(): LocalizationService = object: LocalizationService {
+        override fun getTranslation(key: String, locale: Locale) = key
+    }
+}
