68b041606a469db2e298e016d666fd4099f2cb3c
Fredrik Tõnisson-Bystam <fredrik.bystam@hedvig.com>
2021-03-01 09:46:59 +0100

    remove unused @Valid annotation

--- src/main/kotlin/com/hedvig/underwriter/web/ExternalQuoteController.kt
+++ src/main/kotlin/com/hedvig/underwriter/web/ExternalQuoteController.kt
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import javax.servlet.http.HttpServletRequest
-import javax.validation.Valid
 
 @RestController
 @RequestMapping("/quotes")
@@ -22,7 +21,7 @@ class ExternalQuoteController(
 ) {
     @PostMapping
     fun createQuote(
-        @Valid @RequestBody body: ExternalQuoteRequestDto,
+        @RequestBody body: ExternalQuoteRequestDto,
         httpServletRequest: HttpServletRequest
     ): ResponseEntity<out Any> {
         val quoteInitiatedFrom = when {
