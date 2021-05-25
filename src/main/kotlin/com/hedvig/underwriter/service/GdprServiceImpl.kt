3825202ce72433e6fe1525e95ebcaefa37fcf97b
mightyost <osten@forshed.se>
2021-05-25 14:14:45 +0200

    Added an extra PP contract check to GDPR cleaning job.
    There seems to be members created and signed before Underwriter was born. To not delete these we have added a new extra check to PP for the members to delete.

--- src/main/kotlin/com/hedvig/underwriter/service/GdprServiceImpl.kt
+++ src/main/kotlin/com/hedvig/underwriter/service/GdprServiceImpl.kt
@@ -6,6 +6,7 @@ import com.hedvig.underwriter.service.exceptions.NotFoundException
 import com.hedvig.underwriter.serviceIntegration.apigateway.ApiGatewayService
 import com.hedvig.underwriter.serviceIntegration.memberService.MemberService
 import com.hedvig.underwriter.serviceIntegration.notificationService.NotificationService
+import com.hedvig.underwriter.serviceIntegration.productPricing.ProductPricingService
 import com.hedvig.underwriter.util.logger
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
@@ -19,6 +20,7 @@ class GdprServiceImpl(
     val notificationService: NotificationService,
     val apiGatewayService: ApiGatewayService,
     val memberService: MemberService,
+    val productPricingService: ProductPricingService,
     val quoteRepository: QuoteRepository
 ) : GdprService {
 
@@ -88,7 +90,19 @@ class GdprServiceImpl(
 
         // If a member has no other quotes than in those to clean,
         // then the member can be deleted
-        return memberIds.filter { hasNoOtherQuotes(it, quoteIds) }
+        val candidates = memberIds
+            .filter { hasNoOtherQuotes(it, quoteIds) }
+
+        // Double check with PP if any contract exists for member. This is an extra
+        // safety belt to not delete members that was created and signed before
+        // Underwriter was born...
+        val hasContracts = candidates.filter { hasContracts(it) }
+
+        hasContracts.forEach {
+            logger.warn("Member $it has a contract in PP but it is not registered in Underwriter, skipping it")
+        }
+
+        return candidates - hasContracts
     }
 
     private fun hasNoOtherQuotes(memberId: String, quotes: Set<UUID>): Boolean =
@@ -123,6 +137,15 @@ class GdprServiceImpl(
                 continue
             }
             quoteService.deleteQuote(quote.id)
+        }
+    }
+
+    private fun hasContracts(memberId: String): Boolean {
+        try {
+            return productPricingService.hasContract(memberId)
+        } catch (e: Exception) {
+            logger.error("Failed to check if member $memberId has contracts in Product and Pricing Service: ${e.message}")
+            throw e
         }
     }
 
