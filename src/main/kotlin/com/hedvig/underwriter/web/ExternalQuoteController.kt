f7bc804ed972eff4c843a7899652f3f31c4d4783
Fredrik Tõnisson-Bystam <fredrik.bystam@hedvig.com>
2021-05-26 11:10:39 +0200

    MX-148: enable tag quotes with QuoteInitiatedFrom in the public API, and add SELF_CHANGE to its cases

--- src/main/kotlin/com/hedvig/underwriter/web/ExternalQuoteController.kt
+++ src/main/kotlin/com/hedvig/underwriter/web/ExternalQuoteController.kt
@@ -1,26 +1,22 @@
 package com.hedvig.underwriter.web
 
 import arrow.core.getOrHandle
-import com.hedvig.graphql.commons.extensions.isAndroid
-import com.hedvig.graphql.commons.extensions.isIOS
-import com.hedvig.underwriter.model.QuoteInitiatedFrom
-import com.hedvig.underwriter.service.QuoteService
-import com.hedvig.underwriter.service.model.QuoteRequest
 import com.hedvig.libs.logging.calls.LogCall
 import com.hedvig.productPricingObjects.dtos.SelfChangeResult
+import com.hedvig.underwriter.service.QuoteService
 import com.hedvig.underwriter.service.SelfChangeService
+import com.hedvig.underwriter.service.model.QuoteRequest
 import com.hedvig.underwriter.web.dtos.ExternalQuoteRequestDto
 import com.hedvig.underwriter.web.dtos.QuoteDto
 import java.util.UUID
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
-import javax.servlet.http.HttpServletRequest
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/quotes")
@@ -31,18 +27,11 @@ class ExternalQuoteController(
     @PostMapping
     @LogCall
     fun createQuote(
-        @RequestBody body: ExternalQuoteRequestDto,
-        httpServletRequest: HttpServletRequest
+        @RequestBody body: ExternalQuoteRequestDto
     ): ResponseEntity<out Any> {
-        val quoteInitiatedFrom = when {
-            httpServletRequest.isAndroid() -> QuoteInitiatedFrom.ANDROID
-            httpServletRequest.isIOS() -> QuoteInitiatedFrom.IOS
-            else -> QuoteInitiatedFrom.APP
-        }
-
         return quoteService.createQuote(
             quoteRequest = QuoteRequest.from(body),
-            initiatedFrom = quoteInitiatedFrom,
+            initiatedFrom = body.initiatedFrom,
             underwritingGuidelinesBypassedBy = null,
             updateMemberService = false
         ).bimap(
--- src/main/kotlin/com/hedvig/underwriter/web/dtos/QuoteDto.kt
+++ src/main/kotlin/com/hedvig/underwriter/web/dtos/QuoteDto.kt
@@ -1,6 +1,7 @@
 package com.hedvig.underwriter.web.dtos
 
 import com.hedvig.underwriter.model.Quote
+import com.hedvig.underwriter.model.QuoteInitiatedFrom
 import java.math.BigDecimal
 import java.util.UUID
 
@@ -8,6 +9,7 @@ data class QuoteDto(
     val id: UUID,
     val price: BigDecimal?,
     val currency: String?,
+    val initiatedFrom: QuoteInitiatedFrom,
     val contractId: UUID?,
     val agreementId: UUID?
 ) {
@@ -16,6 +18,7 @@ data class QuoteDto(
             id = quote.id,
             price = quote.price,
             currency = quote.currency,
+            initiatedFrom = quote.initiatedFrom,
             contractId = quote.contractId,
             agreementId = quote.agreementId
         )
